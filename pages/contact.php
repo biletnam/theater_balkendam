4706a6e1d62fe1510055fcb7af8b7e41332d6c36
brandonvdongen <brandonvdongen@hotmail.com>
2017-06-28 10:37:19 +0200

    updates

--- pages/contact.php
+++ pages/contact.php
@@ -13,7 +13,27 @@ require("shared/header.php");
     ?>
 </div>
 <div id="content_container">
-    contact
+    <div class="container">
+        <form id="contact" action="" method="post">
+            <h3>Contact Theater Balkendam</h3>
+            <h4>Neem contact met ons op zodat wij u kunnen helpen.</h4>
+            <fieldset>
+                <input placeholder="Voornaam" type="text" tabindex="1" required autofocus>
+            </fieldset>
+            <fieldset>
+                <input placeholder="E-mail" type="email" tabindex="2" required>
+            </fieldset>
+            <fieldset>
+                <input placeholder="Telefoonnummer" type="tel" tabindex="3" required>
+            </fieldset>
+            <fieldset>
+                <textarea placeholder="Uw bericht" tabindex="5" required></textarea>
+            </fieldset>
+            <fieldset>
+                <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Verstuur</button>
+            </fieldset>
+        </form>
+    </div>
 </div>
 <?php
 require("shared/footer.php");
